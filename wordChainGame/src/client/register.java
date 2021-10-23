07daebac45894a39e63faf3a11e5761002e279c6
Lee JIHO <vc54@naver.com>
2021-10-23 22:07:11 +0900

    register class alpha

--- wordChainGame/src/client/register.java
+++ wordChainGame/src/client/register.java
@@ -10,7 +10,44 @@ public class register extends JFrame{
 	
 	public void setWindow()
 	{
+		mainWindow.setVisible(true);
+		mainWindow.setSize(300, 400);
+		mainWindow.setTitle("Register");
+		mainWindow.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		mainWindow.setLayout(new BorderLayout());
+		mainWindow.setResizable(false);
 		
+		JPanel descriptionPane = new JPanel();
+		JPanel infomationPane = new JPanel();
+		JPanel buttonPane = new JPanel();
+		
+		mainWindow.add(descriptionPane, BorderLayout.NORTH);
+		mainWindow.add(infomationPane, BorderLayout.CENTER);
+		mainWindow.add(buttonPane, BorderLayout.SOUTH);
+		
+		descriptionPane.setLayout(new FlowLayout());
+		JLabel description = new JLabel("Enter infomation to register");
+		descriptionPane.add(description);
+		description.setHorizontalAlignment(JLabel.CENTER);
+		
+		infomationPane.setLayout(new FlowLayout());
+		
+		
+		buttonPane.setLayout(new FlowLayout());
+		JButton Enter = new JButton("Subimt");
+		buttonPane.add(Enter);
+		
+		Enter.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				login loginrSection = new login();
+				
+				loginrSection.setWindow();
+				mainWindow.dispose();
+			}
+		});
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
